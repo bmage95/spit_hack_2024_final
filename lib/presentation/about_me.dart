0323b59ece5a785639651ff8a5ba2418e69ae447
bmage95 <aditya.negi2206@gmail.com>
2024-02-10 22:49:22 +0530

    sm things

--- lib/presentation/about_me.dart
+++ lib/presentation/about_me.dart
@@ -1,15 +1,78 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AboutMe extends StatefulWidget {
-  const AboutMe({super.key});
+  const AboutMe({Key? key}) : super(key: key);
 
   @override
   State<AboutMe> createState() => _AboutMeState();
 }
 
 class _AboutMeState extends State<AboutMe> {
+
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+
+  FirebaseStorage storage = FirebaseStorage.instance;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('About Me'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () {
+                ImagePicker().pickImage(source: ImageSource.gallery).then((value) {
+                  if(value!=null){
+                    File file = File(value.path);
+                    Reference storageRef = storage.refFromURL("gs://spit-hack-2024.appspot.com");
+                    
+                  }
+                });
+              },
+              child: CircleAvatar(
+                radius: 60,
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: _nameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Name',
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Function to save user data
+                    },
+                    child: Text('Save'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
